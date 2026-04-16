bd580647cacd65f81a617c66eebc1d65f899464e
m-kuber <kishore.kuber23@vit.edu>
2026-04-16 06:16:38 -0400

    Add EmotionDetection package

---
+++ EmotionDetection/emotion_detection.py
@@ -0,0 +1,41 @@
+import urllib.request
+import json
+
+def emotion_detector(text_to_analyze):
+    url = "https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict"
+    
+    headers = {
+        "grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"
+    }
+    
+    data = json.dumps({
+        "raw_document": {
+            "text": text_to_analyze
+        }
+    }).encode("utf-8")
+    
+    req = urllib.request.Request(url, data=data, headers=headers)
+    response = urllib.request.urlopen(req)
+    
+    response_text = response.read().decode("utf-8")
+    response_dict = json.loads(response_text)
+    
+    emotions = response_dict["emotionPredictions"][0]["emotion"]
+    
+    anger = emotions["anger"]
+    disgust = emotions["disgust"]
+    fear = emotions["fear"]
+    joy = emotions["joy"]
+    sadness = emotions["sadness"]
+    
+    # Find dominant emotion
+    dominant_emotion = max(emotions, key=emotions.get)
+    
+    return {
+        "anger": anger,
+        "disgust": disgust,
+        "fear": fear,
+        "joy": joy,
+        "sadness": sadness,
+        "dominant_emotion": dominant_emotion
+    }
